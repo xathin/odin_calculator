dcf0a1847d0586062517e218360b201392ab6ff0
Steven Parker <sparker81@gmail.com>
2018-12-16 22:17:01 -0500

    Added operate function

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ function multiply(op1, op2) {
 function divide(op1, op2) {
     return op1 / op2;
 }
+
+function operate(operation, op1, op2) {
+    switch (operation) {
+        case '+':
+            return add(op1, op2);
+            break;
+
+        case '-':
+            return subtract(op1, op2);
+            break;
+
+        case '*':
+            return multiply(op1, op2);
+            break;
+
+        case '/':
+            return divide(op1, op2);
+            break;
+
+        default:
+            break;
+    }
+}
